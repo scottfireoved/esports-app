97b3afd31f1c74becdc74eae4571bc3941f88859
scottfireoved <scottfireoved@gmail.com>
2019-11-02 17:00:07 -0700

    refactored project to use vue routing.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import axios from "axios";
-
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
+  name: 'App'
+}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
